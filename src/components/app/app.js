55de236351c1af53f5db33de41e6827ca4577cf6
Kat <avchimchik@gmail.com>
2025-02-04 17:22:24 +0500

    Пропсы готовы

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
+
+    const data = [
+        {name: 'Вася Пупкин', salary: 800, increase: false},
+        {name: 'Иван Иванов', salary: 3000, increase: true},
+        {name: 'Кирилл Денисов', salary: 5000, increase: false}
+    ]
+
     return (
         <div className="app">
             <AppInfo/>
@@ -15,7 +22,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data= {data}/>
             <EmployeesAddForm/>
         </div>
     );
